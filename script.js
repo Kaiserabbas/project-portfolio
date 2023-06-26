da861630822ed566d4d5b614219da67464256892
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-06-26 14:11:33 +0500

    Modify script for mobile-menu and style changes.

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 function toggleDropdown() {
   const dropdown = document.getElementById('dropdownContent');
   dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-  const bground = document.getElementById('main-page');
-  bground.classList.add('blurr');
+  const bground = document.getElementById('main');
+  bground.classList.add('blur');
   const bgroundLogo = document.getElementById('logo');
-  bgroundLogo.classList.add('blurr');
+  bgroundLogo.classList.add('blur');
 }
 
 function closeDropdown() {
   const dropdown = document.getElementById('dropdownContent');
   dropdown.style.display = 'none';
-  const bground = document.getElementById('main-page');
-  bground.classList.remove('blurr');
+  const bground = document.getElementById('main');
+  bground.classList.remove('blur');
   const bgroundLogo = document.getElementById('logo');
-  bgroundLogo.classList.remove('blurr');
+  bgroundLogo.classList.remove('blur');
 }
 /* eslint-enable no-unused-vars */

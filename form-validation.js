1b6a0bf5e8b905df68b063ce0781189186a42081
Muluken Zemed <zmmulu@gmail.com>
2023-06-28 17:09:56 +0300

    Add event listener to trigger validationCheck function  when the form is sumbitted

--- form-validation.js
+++ form-validation.js
@@ -58,3 +58,8 @@ function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 }
 
+// adding event listener to form section
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	validationCheck();
+});

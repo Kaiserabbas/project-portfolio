2f5ba1764cbc0808b2ae506e5eca3cd42421cf3d
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-06-30 13:21:17 +0500

    Add a code for local storage data preservation

--- local-storage.js
+++ local-storage.js
@@ -1,18 +1,35 @@
-// Variables
+// Declaring Variables to get form 
 const form = document.querySelector('form');
-// Adding event Listener to form variable
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // defining variabled for built in FormData
-  const formData = new FormData(form);    
-  // defining variable for obejct to get enteries from FormData
-  const object = Object.fromEntries(formData);
 
-  // converting object into string
-  const stringObject= JSON.stringify(object);
-  // setting local storage 
-  localStorage.setItem('form', stringObject);
-
-  //page creation where we can confirm data
-  window.location.href = './form-data-check.html';
-});
+  // Declaring Variables to get form inputs
+  const personName = document.querySelector('#personname');
+  const email = document.querySelector('#email');
+  const textArea = document.querySelector('#textarea');
+  
+  // If data is eneterd previously and saved in local storage, retrieve it
+  const localSavedData = JSON.parse(localStorage.getItem('formSaved'));
+  // When the user loads the page, if there is any data in the local storage the input fields are pre-filled with this data. 
+  if (localSavedData) {
+    personName.value = localSavedData.name;
+    email.value = localSavedData.email;
+    textArea.value = localSavedData.message;
+  }
+  
+  // function to preserve data in browser
+  function preserveData(event) {
+    // This function prevent HTML refresh
+    event.preventDefault();
+    // Creating an object for form values to save later in local storage
+    const formSavedData = {
+      name: personName.value,
+      email: email.value,
+      message: textArea.value,
+    };
+    // converting data into string before saving into browser storage
+    const json = JSON.stringify(formSavedData)
+    // It will save the data to local storage
+    localStorage.setItem('formSaved', json);
+  }
+  personName.addEventListener('input', preserveData);
+  email.addEventListener('input', preserveData);
+  textArea.addEventListener('input', preserveData);
